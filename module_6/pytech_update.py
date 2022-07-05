f8aabd9b6782834efa9b771d32558cf38498096d
zerowotj <rcmcki09@gmail.com>
2022-07-04 20:14:57 -0500

    commit

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -5,5 +5,4 @@ client = MongoClient(url)
 db = client.pytech
 
 collection = db["students"]
-collection.update_one({"student_id":1007, "first_name":"Fred", "last_name":"Freddy"})
-result = db.collection.update_one({"student_id":1007}, {"$set": {"last_name": "Smith"}})
+db.collection.update_one({"student_id":1007}, {"$set": {"last_name": "Smith"}})
